432870ca4dd6ad99d55c6ec7ff81948697413e13
Maxim Dolgov <bananafishbone@gmail.com>
2018-10-13 02:37:33 +0300

    minor

--- src/app/actions/counter.ts
+++ src/app/actions/counter.ts
@@ -1,5 +1,14 @@
+import { Action } from '@ngrx/store'
+
+export interface CustomAction extends Action {
+    type: string;
+    payload?: any;
+}
+
+
+
 export default {
-    INCREMENT: 'INCREMENT', 
-    DECREMENT: 'DECREMENT', 
+    INCREMENT: 'INCREMENT',
+    DECREMENT: 'DECREMENT',
     RESET: 'RESET'
 };
